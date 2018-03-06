011ff2b134bec2067766b9e467a23e6878ecbf65
Greta Studier <gretastudier@yahoo.com>
2018-03-06 14:18:28 -0600

    Fixed Hover! Test.py is now has correct graph

--- Test.py
+++ Test.py
@@ -42,80 +42,42 @@ for i in range(0, 29):
         oo_Year_Gender.loc[i, 'Women'] = women
 print oo_Year_Gender.head()
 
-
-
-#Graph gender medal data
-#from bokeh.plotting import figure, output_file, show
-#from bokeh.models import HoverTool, BoxSelectTool, ColumnDataSource
-
+#Graph
 from bokeh.plotting import ColumnDataSource, figure, show, output_file
 from bokeh.models import BoxSelectTool, ColumnDataSource, HoverTool
 
+#Years String
 yrs = oo_Year_Gender.Year
-
 yrs=yrs.apply(str)
 yrs = yrs.tolist()
 print yrs
-#print yrs.apply(str)
 
+#Delete rows where games were cancled
 for i in range(29):
     if oo_Year_Gender.loc[i, 'Men'] == 0:
         oo_Year_Gender.drop([i], axis=0, inplace=True)
         #print "Hello"
 
-TOOLS="hover,save"
-
-#source1 = ColumnDataSource(oo_Year_Gender[['Year', 'Women']])
-#source2 = ColumnDataSource(oo_Year_Gender[['Year', 'Men']])
-
-#CORRRECT?
-# source = ColumnDataSource(data=dict(
-#     x=oo_Year_Gender[['Year']],
-#     y1=oo_Year_Gender[['Women']],
-#     y2=oo_Year_Gender[['Men']],
-# ))
-
-
-# hover = HoverTool(
-#     tooltips=[
-#         #( 'City','@City'),
-#         ( 'Year','@Year'),
-#         ( 'Medal Count','@Women')],)
-# hover2 = HoverTool(
-#     tooltips=[
-#         #( 'City','@City'),
-#         ( 'Year','@Year'),
-#         ( 'Medal Count','@Men')],)
-
 x = oo_Year_Gender.Year
-y1 = oo_Year_Gender.Women
-y2 = oo_Year_Gender.Men
 
-#source=ColumnDataSource(x,y1,y2)
-
-#Data Stuff
-#TOOLS = "hover, save"
+#Figure Stuff
 p = figure(x_range=[1895, 2009], y_range=[-10, 1300], x_axis_label='Year', y_axis_label='Medal Count',  #"pan,box_zoom,reset,save,hover"
-            title="Overall Medal Count by Gender", tools=TOOLS)
-p.line(x=x, y=y1,  line_width=3, color = "navy", alpha=0.3, legend="Womens Medal Count")
-p.line(x=x, y=y2,  line_width=3, color = "green", alpha=0.3, legend="Mens Medal Count")
-p.circle(x=x, y=y1, color='navy', fill_color="white", size=6, legend="Womens Medal Count")
-p.circle(x=x, y=y2,   color='green', fill_color="white", size=6, legend="Mens Medal Count")
+            title="Total Medal Count by Gender per Year")
+p.title.text_font_size = '20px'
+#Women
+y = oo_Year_Gender.Women
+p.line(x=x, y=y,  line_width=3, color = "navy", alpha=0.3, legend="Womens Medal Count")
+c1 = p.circle(x=x, y=y, color='navy', fill_color="white", size=6, legend="Womens Medal Count")
+p.add_tools(HoverTool(renderers=[c1], tooltips=[("Years", "@x"),
+                                                    ("Women", "@y"),]))
+#Men
+y = oo_Year_Gender.Men
+p.line(x=x, y=y,  line_width=3, color = "green", alpha=0.3, legend="Mens Medal Count")
+c2 = p.circle(x=x, y=y,  color='green', fill_color="white", size=6, legend="Mens Medal Count")
+p.add_tools(HoverTool(renderers=[c2], tooltips=[("Years", "@x"),
+                                                    ("Men", "@y"),]))
 p.patch(x=[1915, 1915, 1917, 1917], y=[-10, 1300, 1300, -10], color='black', alpha=0.2)
 p.patch(x=[1939, 1939, 1945, 1945], y=[-10, 1300, 1300, -10], color='black', alpha=0.2)
-
-# #Hover Stuff:
-# hover = p.select_one(HoverTool).tooltips = [
-#     ("Years", "@x"),
-#     ("Women", "@y1"),
-#     ("Men", "@y2"),
-# ]
-
-p.select_one(HoverTool).tooltips = [
-     ('Year', '@x'),
-     ('Women', '@y1'),
-     ('Men', '@y2'),
-]
 
 #Grid Color
 p.grid.grid_line_color = "white"
@@ -125,7 +87,6 @@ p.background_fill_alpha = 0.4
 
 output_file("olympicdata.html")
 show(p)
-
 
 
 #Text Stuff
